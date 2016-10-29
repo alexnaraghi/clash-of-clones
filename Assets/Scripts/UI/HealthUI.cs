de3eeab74de3e3131411bfa7449287fcb2d7cef9
alexnaraghi <alexandernaraghi@gmail.com>
2016-10-29 16:09:53 -0700

    Removed stale death code.

--- Assets/Scripts/UI/HealthUI.cs
+++ Assets/Scripts/UI/HealthUI.cs
@@ -9,39 +9,12 @@ using UnityEngine.UI;
 public class HealthUI : MonoBehaviour
 {
     [SerializeField] private Slider _slider;
-    [SerializeField] private Image _fillImage;
-
-    // A lot of this explosion/death stuff is part of the original Tanks! codebase.
-    // I'd like to see it moved to its own component, it doesn't have much to do with the health display.
-
-    /// <summary>
-    /// Optional.  A prefab that will be instantiated in Awake, then used whenever the tank dies.
-    /// </summary>
-    [SerializeField] private GameObject _explosionPrefab;
-
-    private AudioSource _explosionAudio;
-    private ParticleSystem _explosionParticles;
-
-    /// <summary>
-    /// Has the tank been reduced beyond zero health yet?
-    /// </summary>
-    private bool _dead;                                
+    [SerializeField] private Image _fillImage;                              
 
     private Entity _entity;
 
     private void Awake()
     {
-        if(_explosionPrefab != null)
-        {
-            // Instantiate the explosion prefab and get a reference to the particle system on it.
-            _explosionParticles = Instantiate(_explosionPrefab).GetComponent<ParticleSystem>();
-        
-            // Get a reference to the audio source on the instantiated prefab.
-            _explosionAudio = _explosionParticles.GetComponent<AudioSource>();
-
-            // Disable the prefab so it can be activated when it's required.
-            _explosionParticles.gameObject.SetActive(false);
-        }
 
         _entity = GetComponent<Entity>();
         Assert.IsNotNull(_entity);
@@ -58,12 +31,6 @@ public class HealthUI : MonoBehaviour
         SetHealthUI();
     }
 
-    private void OnEnable()
-    {
-        // When the tank is enabled, reset the tank's health and whether or not it's dead.
-        _dead = false;
-    }
-
 
     private void onDamageTaken()
     {
@@ -71,12 +38,6 @@ public class HealthUI : MonoBehaviour
         {
             // Change the UI elements appropriately.
             SetHealthUI();
-
-            // If the current health is at or below zero and it has not yet been registered, call OnDeath.
-            if (_entity.HP <= 0 && !_dead)
-            {
-                OnDeath();
-            }
         }
     }
 
@@ -87,29 +48,6 @@ public class HealthUI : MonoBehaviour
         {
             // Set the slider's value appropriately.
             _slider.value = _entity.HP;
-        }
-    }
-
-
-    private void OnDeath()
-    {
-        // Set the flag so that this function is only called once.
-        _dead = true;
-
-        if(_explosionParticles != null)
-        {
-            // Move the instantiated explosion prefab to the tank's position and turn it on.
-            _explosionParticles.transform.position = transform.position;
-            _explosionParticles.gameObject.SetActive(true);
-
-            // Play the particle system of the tank exploding.
-            _explosionParticles.Play();
-        }
-
-        if(_explosionAudio)
-        {
-            // Play the tank explosion sound effect.
-            _explosionAudio.Play();
         }
     }
 }
