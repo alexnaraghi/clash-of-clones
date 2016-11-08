6142b49cf93bd8c61169c38c10b767b7eb0d098f
alexnaraghi <alexandernaraghi@gmail.com>
2016-11-07 23:47:36 -0800

    Added distributed placement for ghost armies.

---
+++ Assets/Scripts/DistributedObjects.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Spawner for cards that have multiple characters.
+/// </summary>
+public class DistributedObjects : MonoBehaviour 
+{
+    [SerializeField] private int _numEntities;
+    [SerializeField] private float _spawnRadius;
+    [SerializeField] private GameObject _prefabToSpawn;
+
+    void Start()
+    {
+        spawnObjects();
+    }
+
+    private void spawnObjects()
+    {
+        for(int i = 0; i < _numEntities; i++)
+        {
+            float angle = (((float)i) / _numEntities) * 360f;
+            Vector3 pos = Utils.GetPointOnCircle(transform.position, _spawnRadius, angle);
+
+            if(_prefabToSpawn != null)
+            {
+                var go = Instantiate(_prefabToSpawn);
+                go.transform.position = pos;
+                go.transform.SetParent(transform);
+                GameModel.Instance.MyPlayer.RotateForPlayer(go);
+            }
+        }
+    }
+}
