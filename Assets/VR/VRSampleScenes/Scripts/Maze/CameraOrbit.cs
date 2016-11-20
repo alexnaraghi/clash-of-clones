58fef50c1bcdbfdc66614a789e9b87639b6247be
alexnaraghi <alexandernaraghi@gmail.com>
2016-11-19 18:15:22 -0800

    Added camera orbit with swipe.

--- Assets/VR/VRSampleScenes/Scripts/Maze/CameraOrbit.cs
+++ Assets/VR/VRSampleScenes/Scripts/Maze/CameraOrbit.cs
@@ -24,7 +24,6 @@ namespace VRStandardAssets.Maze
         [SerializeField] private float m_RotationFadeDuration = 0.2f;       // If fading is enabled, this is the duration of the the fade.
         [SerializeField] private VRCameraFade m_CameraFade;                 // Optional reference to the camera fade script, only required if fading is enabled.
         [SerializeField] private VRInput m_VrInput;                         // Reference to the VRInput to subscribe to swipe events.
-        [SerializeField] private MazeGameController m_MazeGameController;   // Reference to the game controller so swiping will only be handled while the game is playing.
         [SerializeField] private Rigidbody m_Rigidbody;                     // Reference to the camera's rigidbody.
 
 
@@ -52,10 +51,6 @@ namespace VRStandardAssets.Maze
       
         private void HandleSwipe(SwipeDirection swipeDirection)
         {
-            // If the game isn't playing or the camera is fading, return and don't handle the swipe.
-            if (!m_MazeGameController.Playing)
-                return;
-
             if (m_CameraFade.IsFading)
                 return;
 
