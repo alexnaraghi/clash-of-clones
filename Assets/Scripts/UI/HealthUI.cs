c24657cddf6bffb0fad48196b05bd97cda480316
alexnaraghi <alexandernaraghi@gmail.com>
2016-11-20 21:55:58 -0800

    UI and particle optimizations.

--- Assets/Scripts/UI/HealthUI.cs
+++ Assets/Scripts/UI/HealthUI.cs
@@ -5,28 +5,54 @@ using UnityEngine.UI;
 /// <summary>
 /// Gui for a slider based health display.
 /// </summary> 
-[RequireComponent(typeof(Entity))]
 public class HealthUI : MonoBehaviour
 {
-    [SerializeField] private Slider _slider;
-    [SerializeField] private Image _fillImage;                              
-
-    private Entity _entity;
+    [SerializeField] private GameObject _sliderPrefab;                           
+    [SerializeField] private float _sliderScale = 5f;                           
+    [SerializeField] private Entity _entity;
+    
+    private Slider _slider;
+    private Image _fillImage;                           
 
     private void Awake()
     {
-
-        _entity = GetComponent<Entity>();
         Assert.IsNotNull(_entity);
+        Assert.IsNotNull(_sliderPrefab);
 
-        _entity.InitializedEvent.AddListener(init);
+        if(_sliderPrefab != null)
+        {
+            var go = Instantiate(_sliderPrefab);
+            if(go != null)
+            {
+                go.transform.SetParent(transform, worldPositionStays: false);
+                go.transform.localScale = Vector3.one * _sliderScale;
+                _slider = go.GetComponentInChildren<Slider>();
+                _fillImage = go.GetComponentInChildren<Image>();
+
+                Assert.IsNotNull(_slider);
+                Assert.IsNotNull(_fillImage);
+            }
+        }
+
+        if(_entity != null)
+        {
+            _entity.InitializedEvent.AddListener(init);
+        }
     }
 
     public void init()
     {
         _entity.DamageTakenEvent.AddListener(onDamageTaken);
-        _slider.maxValue = _entity.MaxHP;
-        _fillImage.color = _entity.Owner.PlayerColor;
+
+        if(_slider != null)
+        {
+            _slider.maxValue = _entity.MaxHP;
+        }
+        
+        if(_fillImage != null)
+        {
+            _fillImage.color = _entity.Owner.PlayerColor;
+        }
 
         SetHealthUI();
     }
@@ -44,7 +70,7 @@ public class HealthUI : MonoBehaviour
 
     private void SetHealthUI()
     {
-        if (_entity != null)
+        if (_entity != null && _slider != null)
         {
             // Set the slider's value appropriately.
             _slider.value = _entity.HP;
